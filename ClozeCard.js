1dfbee92da5ad36612bba9c6d53e95289239de84
Burke Simmons <burkesimmons@gmail.com>
2017-09-02 19:06:10 -0600

    Updated .partial to log error and got rid of 'undefined' error

--- ClozeCard.js
+++ ClozeCard.js
@@ -4,9 +4,7 @@ var BasicCard = require('./BasicCard.js');
 var ClozeCard = function(text, cloze){
 	this.text = text;
 	this.cloze = cloze;
-	// if(this.text.search(this.cloze)===false){
-	// 	console.log('error');
-	// }
+	
 };
 
 ClozeCard.prototype.fullText = function() {
@@ -15,35 +13,27 @@ ClozeCard.prototype.fullText = function() {
 
 
 ClozeCard.prototype.partial = function() {
+	if(this.text.search(this.cloze)){
+		console.log('error');
+	} else{
 	var clozeLength = this.cloze.length;
 	console.log(' ... ' + this.text.substring(clozeLength));
+	};
 };
 
 var firstPresident = new BasicCard(
     "Who was the first president of the United States?", "George Washington");
 
-// "Who was the first president of the United States?"
-// console.log(firstPresident.front); 
-
-// "George Washington"
-// console.log(firstPresident.back); 
-
 var firstPresidentCloze = new ClozeCard(
     "George Washington was the first president of the United States.", "George Washington");
 
-// "George Washington"
-console.log(firstPresidentCloze.cloze); 
-
-// " ... was the first president of the United States.
-// console.log(firstPresidentCloze.partial);
-
-// "George Washington was the first president of the United States.
-console.log(firstPresidentCloze.fullText());
-console.log(firstPresidentCloze.partial());
+firstPresidentCloze.fullText();
+firstPresidentCloze.partial();
 
 // // Should throw or log an error because "oops" doesn't appear in "This doesn't work"
 var brokenCloze = new ClozeCard("This doesn't work", "oops");
-console.log(brokenCloze.cloze);
-console.log(brokenCloze.partial());
+// console.log(brokenCloze.cloze);
+brokenCloze.partial();
 
-// module.exports = ClozeCard;
+//What is the purpose of this? Are we supposed to have another js file that uses ClozeCard?
+module.exports = ClozeCard;
